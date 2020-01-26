5d8e787d7a12d60ffca2dfa8075b7db0805fea6e
DaYmiHn <sawi.denis@yandex.ru>
2020-01-26 14:45:06 +0300

    echo product

--- untitled/mainwindow.cpp
+++ untitled/mainwindow.cpp
@@ -2,6 +2,7 @@
 #include "ui_mainwindow.h"
 #include "QtSql/QSqlDatabase"
 #include "QSqlQuery"
+#include <QString>
 
 
 MainWindow::MainWindow(QWidget *parent) :
@@ -9,10 +10,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    QSqlDatabase db;
-    db = QSqlDatabase::addDatabase("QSQLITE");
-    db.setDatabaseName("D:\\1.db3");
-    db.open();
+
 }
 
 MainWindow::~MainWindow()
@@ -22,19 +20,33 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_clicked()
 {
-
+    QSqlDatabase db;
+    db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName("C:\\qt_project\\magaz\\database.db3");
+    db.open();
 
     //Осуществляем запрос
     QSqlQuery query;
-    query.exec("SELECT * FROM contacts");
-
+    query.exec("SELECT * FROM product");
+    int count = 0;
     //Выводим значения из запроса
     while (query.next())
     {
-    QString id = query.value(0).toString();
-    QString name = query.value(1).toString();
-    QString age = query.value(2).toString();
-    ui->textEdit->insertPlainText(id+" "+name+" "+age+"\n");
+//    QString id = query.value(1).toString();
+//    count = QString::number(query.size());
+//    ui->textEdit->insertPlainText(id+"\n");
+    count++;
     }
-    ui->statusBar->showMessage("Вывел");
+    ui->statusBar->showMessage( QString::number(count));
+    ui->tableWidget->setRowCount(count);
+    ui->tableWidget->setColumnCount(3);
+    query.first();
+    for(int i=0; i<count; i++){
+        for(int j=0; j<3; j++){
+            ui->tableWidget->setItem(i, j, new QTableWidgetItem(query.value(j).toString()));
+        }
+        query.next();
+    }
+
 }
+
